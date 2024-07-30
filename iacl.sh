c2defe57519988aaf05964280ab49e0784cf0289
Felipe P Zelent <84643253+FelipeZelent@users.noreply.github.com>
2024-07-30 17:36:10 -0300

    Update iacl.sh

--- iacl.sh
+++ iacl.sh
@@ -5,36 +5,38 @@ echo "Criando diretórios..."
 mkdir /publico
 mkdir /adm
 mkdir /ven
-mkdir /sec
+mkdir /ifa
 
 echo "Criando grupos de usuários..."
 
 groupadd GRP_ADM
 groupadd GRP_VEN
-groupadd GRP_SEC
+groupadd GRP_IFA
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd julio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+useradd mariana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM
+
 useradd debora -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd renata -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
 useradd roberto -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
-useradd josefina -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+
+useradd ana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_IFA
+useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_IFA
+useradd giovana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_IFA
 
 
 echo "Especificando permissões dos diretórios..."
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
-chown root:GRP_SEC /sec
+chown root:GRP_IFA /ifa
 
 chmod 770 /adm
 chmod 770 /ven
-chmod 770 /sec
+chmod 770 /ifa
 chmod 777 /publico
 
 echo "Fim..."
