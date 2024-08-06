70531372ea5f6964c7b5bb2d3f7f35ac9847f7a9
Felipe P Zelent <84643253+FelipeZelent@users.noreply.github.com>
2024-08-06 09:34:52 -0300

    Apache_server iacl.sh

--- iacl.sh
+++ iacl.sh
@@ -40,3 +40,21 @@ chmod 770 /ifa
 chmod 777 /publico
 
 echo "Fim..."
+
+echo "Apache-server-linux"
+
+#!/bin/bash
+
+echo "Att Server"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "baixando zip"
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
